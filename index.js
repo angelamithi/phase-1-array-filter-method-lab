7095c9f5ad2b46b7bc2f2aec90b067836af94133
+ <+>
2023-09-24 11:02:59 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution here
+const findMatching= function(drivers,name='Bobby'){
+   const newDriversArray=[];
+    for (const driver of drivers){
+    if (driver.toUpperCase() === name.toUpperCase())
+    {
+        newDriversArray.push(driver)
+    }
+}
+    return newDriversArray;
+   }
+   
+const fuzzyMatch = function(drivers,name='Sa'){
+const newDriversArray=[];
+for(const driver of drivers){
+    if(driver.slice(0,2)===name){
+        newDriversArray.push(driver);
+    }
+}
+return newDriversArray;
+
+}
+
+function matchName(drivers,name='Bobby'){
+    const newDriversArray=[];
+    for(const driver of drivers){
+        if(driver.name===name){
+            newDriversArray.push(driver)
+        }
+    }
+    return newDriversArray;
+
+}
